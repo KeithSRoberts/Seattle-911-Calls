d8dbe48939a54bd638b4d073b138c4413cff0916
mmb36 <32443390+mmb36@users.noreply.github.com>
2017-12-05 21:15:15 -0800

    Add files via upload

--- server.R
+++ server.R
@@ -6,13 +6,15 @@ library("dplyr")
 library("leaflet")
 library("shinythemes")
 library("maps")
+library('tidyr')
+library('bit64')
 library(ggplot2)
 library(data.table)
 
 server <- function(input, output) {
   #callData is the original csv
   callData <- fread("data/Seattle_Police_Department_911_Incident_Response.csv", header = TRUE)
-
+  
   
   ## KEITH's SERVER FUNCTIONS
   #Reactive Function for filtering widgets
@@ -32,34 +34,50 @@ server <- function(input, output) {
   })
   #Observer function for searching an address
   observeEvent(input$go, {
-      search <- filter(callData, grepl(input$search, `Hundred Block Location`))
-      leafletProxy("map") %>%
-        setView(lng = search$Longitude[1], lat = search$Latitude[1], zoom = 15)
+    search <- filter(callData, grepl(input$search, `Hundred Block Location`))
+    leafletProxy("map") %>%
+      setView(lng = search$Longitude[1], lat = search$Latitude[1], zoom = 15)
   })
   
   ##Min's server function
   #reactive function for filtering data using input values (month and year)
   crime.group <- reactive({
-      #remove time from date column 
-      if (callData[1,] %>% select(`Event Clearance Date`)== format('%m/%d/%Y %H:%M')) {
-        callData$`Event Clearance Date` <- format(as.POSIXct(callData$`Event Clearance Date`, format = '%m/%d/%Y %H:%M'), format = '%m/%d/%Y')
-      }
-      #filter month and year count frequency of each crime type 
-      crime.type <- callData %>% 
-        filter(format(as.Date(callData$`Event Clearance Date`, "%m/%d/%Y"), "%m") == input$month &
-                 format(as.Date(callData$`Event Clearance Date`, "%m/%d/%Y"), "%Y") == input$year) %>%
-        group_by(`Event Clearance SubGroup`) %>%
-        summarize(frequency = n())
-      
-    })
-   #display histogram
-    output$hist <- renderPlot({
-      ggplot(crime.group(), aes(x = `Event Clearance SubGroup`, y = frequency)) +
-        geom_bar(stat = "identity") + labs(x = "Crime Groups", y = "Crime Frequency") + 
-        ggtitle(paste0(input$month, "/",input$year, " crime type frequencies")) + coord_flip()
-    })
-    #display stat table
-    output$stat <- renderTable({
-      crime.group() %>% arrange(desc(frequency))
-    })
+    #remove time from date column 
+    if (callData[1,] %>% select(`Event Clearance Date`)== format('%m/%d/%Y %H:%M')) {
+      callData$`Event Clearance Date` <- format(as.POSIXct(callData$`Event Clearance Date`, format = '%m/%d/%Y %H:%M'), format = '%m/%d/%Y')
+    }
+    #filter month and year count frequency of each crime type 
+    crime.type <- callData %>% 
+      filter(format(as.Date(callData$`Event Clearance Date`, "%m/%d/%Y"), "%m") == input$month &
+               format(as.Date(callData$`Event Clearance Date`, "%m/%d/%Y"), "%Y") == input$year) %>%
+      group_by(`Event Clearance SubGroup`) %>%
+      summarize(frequency = n())
+    
+  })
+  #display histogram
+  output$hist <- renderPlot({
+    ggplot(crime.group(), aes(x = `Event Clearance SubGroup`, y = frequency)) +
+      geom_bar(stat = "identity") + labs(x = "Crime Groups", y = "Crime Frequency") + 
+      ggtitle(paste0(input$month, "/",input$year, " crime type frequencies")) + coord_flip()
+  })
+  #display stat table
+  output$stat <- renderTable({
+    crime.group() %>% arrange(desc(frequency))
+  })
+  output$CrimePlot <- renderPlot({
+    crime_data <- separate(callData, `Event Clearance Date`, c('Date', 'Time'), sep = ' ') %>%
+      separate(`Date`, c('Month', 'Day', 'Year'), sep = '/')
+    crime_data <- rename(crime_data, Type = `Event Clearance Group`)  
+    data <- filter(crime_data, `Year` == input$YearInput, `Type` == input$CrimeTypeInput)
+    monthly <-  
+      group_by(data,`Month`) %>% 
+      summarize(Frequency = n())
+    monthly.chart <- ggplot(data = monthly, mapping = aes(x = Month, y = Frequency )) + 
+      geom_point() +
+      ggtitle('Crimes Per Month')
+    print(monthly.chart)
+    
+  })
 }
+
+
--- ui.R
+++ ui.R
@@ -3,86 +3,95 @@
 ## R script for UI of the Shiny App
 
 ui <- fluidPage(theme = shinytheme("sandstone"),
-  #Page Title
-  navbarPage("Seattle Area 911 Calls",
-             #Overview Tab <--- Assigned to Deanna
-             #Details on what to include in our overview are in the README
-             tabPanel("Overview",
-              titlePanel("911 Calls in Seattle - About the Project"),
-              #This Tab is linked to our README.md, so we only need to modify the README
-              includeMarkdown("README.md")
-             ),
-             
-             #Map of Calls Tab <--- Assigned to Keith
-             #Widgets to include:
-             # - Search by address / SidePanel
-             # - Filter by crime / Side Panel
-             #Main Page:
-             # - Include map that plots the crimes from the data set
-             tabPanel("Map of Calls",
-              titlePanel("Visual Map of Calls In Seattle"),
-              sidebarLayout(
-                sidebarPanel(
-                 # Put search by address widget here
-                 textInput("search", h3("Search Map"), value = "Enter Address"),
-                 actionButton("go", "Search"),
-                 # Put filter by crime widget here  
-                 selectInput("crime", label = h3("Choose Crime"), choices = sort(unique(callData$`Event Clearance Group`)), selected = "ACCIDENT INVESTIGATION"),
-                 # Year Filter
-                 selectInput("years", label = h3("Select Year"), choices = c("ALL" = "8", "2010" = "0", "2011" = "1", "2012" = "2", "2013" = "3", "2014" = "4", "2015" = "5", "2016" = "6", "2017"= "7"))
-               ),
-               mainPanel(
-                 # Put Leaflet Map here
-                 h3("Map of 911 Calls"),
-                 leafletOutput("map", height = "400px")
-               )
-              )
-             ),
-             
-             #Calls Overtime Graph Tab <--- Assigned to Matthew
-             #Widgets:
-             # - Radio Buttons to Select Year / SidePanel
-             # - Dropdown Menu to select Crime type / SidePanel
-             #Main Panel:
-             # - Graph of frequency in crime for each month of selected year and crime type
-             tabPanel("Calls Overtime",
-              titlePanel("Visualization of 911 Calls Overtime"),        
-              sidebarLayout(
-                sidebarPanel(
-                  #Radio button widget here
-                 
-                  #Dropdown crime select here
-                ),
-                mainPanel(
-                  #Crime Graph here          
-               )
-              )
-             ),
-             
-             #Calls by Crime Tab <--- Assigned to Min
-             #Widgets: 
-             # - Dropdown to select month for graph
-             #MainPanel:
-             # - Include a graph showing frequency of crimes for chosen month
-             tabPanel("Calls by Crime",
-              titlePanel("Calls by Crime in the Seattle Area"),
-              sidebarLayout(
-               sidebarPanel(
-                 #Dropdown year and month select widget here
-                 selectInput("year", label = h3("Select Year"), 
-                             choices = list("2010"= "2010", "2011"="2011","2012"="2012","2014"="2014",
-                                            "2015"="2015", "2016"="2016", "2017"="2017"), selected = "2010"),
-                 selectInput("month", label = h3("Select Month"), 
-                             choices = list("January" = "01", "Feburary" = "02", "March" = "03", "April" = "04",
-                                            "May" ="05", "June"='6',"July"="07","August"='08',"September"='09',
-                                            "October"='10', "November"='11',"December"='12'), selected = "July"),
-                 tableOutput("stat")
-               ),
-               mainPanel(
-                  #Graph of frequency -> crime here 
-                 plotOutput("hist")
-               )
-              )
-             )
-  )
+                #Page Title
+                navbarPage("Seattle Area 911 Calls",
+                           #Overview Tab <--- Assigned to Deanna
+                           #Details on what to include in our overview are in the README
+                           tabPanel("Overview",
+                                    titlePanel("911 Calls in Seattle - About the Project"),
+                                    #This Tab is linked to our README.md, so we only need to modify the README
+                                    includeMarkdown("README.md")
+                           ),
+                           
+                           #Map of Calls Tab <--- Assigned to Keith
+                           #Widgets to include:
+                           # - Search by address / SidePanel
+                           # - Filter by crime / Side Panel
+                           #Main Page:
+                           # - Include map that plots the crimes from the data set
+                           tabPanel("Map of Calls",
+                                    titlePanel("Visual Map of Calls In Seattle"),
+                                    sidebarLayout(
+                                      sidebarPanel(
+                                        # Put search by address widget here
+                                        textInput("search", h3("Search Map"), value = "Enter Address"),
+                                        actionButton("go", "Search"),
+                                        # Put filter by crime widget here  
+                                        selectInput("crime", label = h3("Choose Crime"), choices = sort(unique(callData$`Event Clearance Group`)), selected = "ACCIDENT INVESTIGATION"),
+                                        # Year Filter
+                                        selectInput("years", label = h3("Select Year"), choices = c("ALL" = "8", "2010" = "0", "2011" = "1", "2012" = "2", "2013" = "3", "2014" = "4", "2015" = "5", "2016" = "6", "2017"= "7"))
+                                      ),
+                                      mainPanel(
+                                        # Put Leaflet Map here
+                                        h3("Map of 911 Calls"),
+                                        leafletOutput("map", height = "400px")
+                                      )
+                                    )
+                           ),
+                           
+                           #Calls Overtime Graph Tab <--- Assigned to Matthew
+                           #Widgets:
+                           # - Radio Buttons to Select Year / SidePanel
+                           # - Dropdown Menu to select Crime type / SidePanel
+                           #Main Panel:
+                           # - Graph of frequency in crime for each month of selected year and crime type
+                           tabPanel("Calls Overtime",
+                                    titlePanel("Visualization of 911 Calls Overtime"),        
+                                    sidebarLayout(
+                                      sidebarPanel(
+                                        selectInput(inputId = "YearInput", label = "Choose a Year:", 
+                                                    choices = c('2010' = 10, 
+                                                                '2011' = 11, 
+                                                                '2012' = 12,
+                                                                '2013' = 13,
+                                                                "2014" = 14,
+                                                                "2015" = 15,
+                                                                "2016" = 16,
+                                                                "2017" = 17),
+                                                    selected = '2017'),
+                                        selectInput(inputId= "CrimeTypeInput", label = "Select a crime:", 
+                                                    choices = sort(unique(callData$`Event Clearance Group`)), selected = "ACCIDENT INVESTIGATION")
+                                      ),
+                                      mainPanel(
+                                        plotOutput("CrimePlot")
+                                      )
+                                    )
+                           ),
+                           
+                           #Calls by Crime Tab <--- Assigned to Min
+                           #Widgets: 
+                           # - Dropdown to select month for graph
+                           #MainPanel:
+                           # - Include a graph showing frequency of crimes for chosen month
+                           tabPanel("Calls by Crime",
+                                    titlePanel("Calls by Crime in the Seattle Area"),
+                                    sidebarLayout(
+                                      sidebarPanel(
+                                        #Dropdown year and month select widget here
+                                        selectInput("year", label = h3("Select Year"), 
+                                                    choices = list("2010"= "2010", "2011"="2011","2012"="2012","2014"="2014",
+                                                                   "2015"="2015", "2016"="2016", "2017"="2017"), selected = "2010"),
+                                        selectInput("month", label = h3("Select Month"), 
+                                                    choices = list("January" = "01", "Feburary" = "02", "March" = "03", "April" = "04",
+                                                                   "May" ="05", "June"='6',"July"="07","August"='08',"September"='09',
+                                                                   "October"='10', "November"='11',"December"='12'), selected = "July"),
+                                        tableOutput("stat")
+                                      ),
+                                      mainPanel(
+                                        #Graph of frequency -> crime here 
+                                        plotOutput("hist")
+                                      )
+                                    )
+                           )
+                )
 )
